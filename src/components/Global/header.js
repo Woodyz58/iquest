fd8988ffece12f2876af4eda576aebd0413d19bf
Joe <joe@startdigital.com.au>
2021-09-06 15:42:59 +0800

    temporarily remove the mobile nav menu

--- src/components/Global/header.js
+++ src/components/Global/header.js
@@ -6,19 +6,7 @@ import menuJson from "../../../content/menu.json"
 /* This example requires Tailwind CSS v2.0+ */
 import { Fragment } from 'react'
 import { Popover, Transition } from '@headlessui/react'
-import {
-  BookmarkAltIcon,
-  CalendarIcon,
-  ChartBarIcon,
-  CursorClickIcon,
-  MenuIcon,
-  PhoneIcon,
-  PlayIcon,
-  SupportIcon,
-  ViewGridIcon,
-  XIcon,
-} from '@heroicons/react/outline'
-// import { ChevronDownIcon } from '@heroicons/react/solid'
+import { MenuIcon, XIcon } from '@heroicons/react/outline'
 import MenuPopover from "../MenuPopover"
 
 export default function Header() {
@@ -49,20 +37,20 @@ export default function Header() {
                     <MenuPopover key={title} title={title} menu={menuItem[1]} />
                   )
                 })}
-                <a href="../case-studies" className="text-base font-medium text-white hover:text-white-900">Case Studies</a>
+                <a href="/case-studies" className="text-base font-medium text-white hover:text-white-900">Case Studies</a>
               </Popover.Group>
               <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
-                <a
-                  href="#"
+                <Link
+                  to="#"
                   className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700"
                 >
                   Contact Us
-                </a>
+                </Link>
               </div>
             </div>
           </div>
 
-          {/* <Transition
+          <Transition
             show={open}
             as={Fragment}
             enter="duration-200 ease-out"
@@ -91,11 +79,18 @@ export default function Header() {
                   </div>
                   <div className="mt-6">
                     <nav className="grid gap-y-8">
-                      {menuItems.map((item) => (
-                        <a key={item.name} href={item.href} className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50">
-                          <item.icon className="flex-shrink-0 h-6 w-6 text-indigo-600" aria-hidden="true" />
-                          <span className="ml-3 text-base font-medium text-gray-900">{item.name}</span></a>
-                      ))}
+                      Add mobile menu items here
+                      {/* {menuItems.map((menuItem) => {
+                        const items = menuItem[1];
+                        items.map(item => {
+                          return (
+                            <Link key={item.name} href={item.href} className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50" >
+                              <item.icon className="flex-shrink-0 h-6 w-6 text-indigo-600" aria-hidden="true" />
+                              <span className="ml-3 text-base font-medium text-gray-900">{item.name}</span>
+                            </Link>
+                          )
+                        })
+                      })} */}
                     </nav>
                   </div>
                 </div>
@@ -113,7 +108,7 @@ export default function Header() {
                 </div>
               </div>
             </Popover.Panel>
-          </Transition> */}
+          </Transition>
         </>
       )
       }
