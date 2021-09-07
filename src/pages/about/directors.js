887ad969a43974dd768cb942cbe1447f35b73984
Joe <joe@startdigital.com.au>
2021-09-07 16:17:37 +0800

    properly structure the internal page layout template

--- src/pages/about/directors.js
+++ src/pages/about/directors.js
@@ -8,28 +8,24 @@ export default function Directors() {
     <>
       <PageTransition>
         <InternalLayout title="Our Directors" description="We're Perth's leading IT consulting company, ready to deploy experts anytime & anywhere.">
-          <div className="relative bg-white overflow-hidden">
-            <div className="max-w-6xl mx-auto">
-              <div className="relative z-10 pt-16 pb-28 px-4 sm:px-6 lg:px-8 grid lg:grid-cols-2 gap-16">
-                <div className="flex flex-col space-y-6">
-                  <h2 className="mb-0">
-                    Anderson Chong<br />
-                    <span className="uppercase text-base tracking-wide text-blue-dark font-bold">Managing Director</span>
-                  </h2>
-                  <p>
-                    Our partnerships with these companies are advantageous for our clients as it’s through our relationships that we can offer better pricing and service. Our close relationships with our partners also means we can build upon our knowledge and seek answers quickly on issues of hardware and software.
-                  </p>
-                  <p>
-                    By having these extremely experienced and influential companies providing us with resources we can provide a range of Technology Consulting activities to you, including Strategic IT planning, Application Software Development, and Business Analyst roles with highly experienced senior IT Consultants by your side. Through iQuest, we specialise in assisting small, medium and corporate businesses to get the most out of their IT systems.
-                  </p>
-                </div>
-                <StaticImage
-                  className="w-full rounded-xl shadow-xl lg:w-auto"
-                  src="../../images/iQuest-Anderson.jpeg"
-                  alt="iQuest About Us Banner"
-                  placeholder="blurred" />
-              </div>
+          <div className="relative z-10 px-4 sm:px-6 lg:px-8 grid lg:grid-cols-2 gap-16">
+            <div className="flex flex-col space-y-6">
+              <h2 className="mb-0">
+                Anderson Chong<br />
+                <span className="uppercase text-base tracking-wide text-blue-dark font-bold">Managing Director</span>
+              </h2>
+              <p>
+                Our partnerships with these companies are advantageous for our clients as it’s through our relationships that we can offer better pricing and service. Our close relationships with our partners also means we can build upon our knowledge and seek answers quickly on issues of hardware and software.
+              </p>
+              <p>
+                By having these extremely experienced and influential companies providing us with resources we can provide a range of Technology Consulting activities to you, including Strategic IT planning, Application Software Development, and Business Analyst roles with highly experienced senior IT Consultants by your side. Through iQuest, we specialise in assisting small, medium and corporate businesses to get the most out of their IT systems.
+              </p>
             </div>
+            <StaticImage
+              className="w-full rounded-xl shadow-xl lg:w-auto"
+              src="../../images/iQuest-Anderson.jpeg"
+              alt="iQuest About Us Banner"
+              placeholder="blurred" />
           </div>
         </InternalLayout>
       </PageTransition>
---
+++ src/pages/it-support/it-support-services-perth.js
@@ -0,0 +1,17 @@
+import * as React from "react"
+import PageTransition from 'gatsby-plugin-page-transitions';
+import InternalLayout from "../../components/layout/InternalLayout"
+
+function ITSupportServicesPerth() {
+  return (
+    <>
+      <PageTransition>
+        <InternalLayout title="IT Support Services Perth" description="Providing fully managed IT support services in Perth & Melbourne.">
+          hey
+        </InternalLayout>
+      </PageTransition>
+    </>
+  )
+}
+
+export default ITSupportServicesPerth
