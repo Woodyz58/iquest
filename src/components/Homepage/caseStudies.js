b3a75616ce4a09f2b0a26e3ad35120bb4ded0e87
Joe <joe@startdigital.com.au>
2021-09-07 14:09:26 +0800

    minor component updates

--- src/components/Homepage/caseStudies.js
+++ src/components/Homepage/caseStudies.js
@@ -1,36 +1,36 @@
-import * as React from "react"
+import React from "react"
+import { Link } from "gatsby"
 import casestudy1 from "/src/images/iQuest-Client-Singtel.jpg"
 import casestudy2 from "/src/images/iQuest-Client-SLSC.jpg"
 import casestudy3 from "/src/images/iQuest-Client-Kwik-Logistics.jpg"
 
 export default function CaseStudies() {
-    return (
-      <div className="bg-white">
-        <div className="max-w-7xl mx-auto py-16 px-4 sm:pt-12 sm:pb-24 sm:px-6 lg:px-8">
-          <div className="text-center">
-            <p className="text-base font-semibold text-indigo-600 tracking-wide uppercase"></p>
-            <h2 className="text-5xl font-extrabold tracking-tight text-gray-900">iQuest Case Studies</h2>
-            <p className="max-w-xl mt-2 mx-auto text-xl text-gray-600">
+  return (
+    <div className="bg-white">
+      <div className="max-w-7xl mx-auto py-16 px-4 sm:pt-12 sm:pb-24 sm:px-6 lg:px-8">
+        <div className="text-center">
+          <p className="text-base font-semibold text-indigo-600 tracking-wide uppercase"></p>
+          <h2 className="text-5xl font-extrabold tracking-tight text-gray-900">iQuest Case Studies</h2>
+          <p className="max-w-xl mt-2 mx-auto text-xl text-gray-600">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore.
-            </p>
-            <div className="mt-6"><a href="#" className="inline-flex px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700">View all case studies</a></div>
+          </p>
+          <div className="mt-6"><Link to="#" className="btn btn-blue">View all case studies</Link></div>
+        </div>
+        <div className="flex flex-wrap mt-12">
+          <div className=" md:w-1/3 sm:px-6 w-full mb-10">
+            <img className="mx-auto" src={casestudy1} width="350" height="347" alt="Singtel Casestudy" />
+            <div className="mt-6 text-center"><Link to="#" className="btn btn-small btn-blue">View case study</Link></div>
           </div>
-            <div className="flex flex-wrap mt-12">
-                <div className=" md:w-1/3 sm:px-6 w-full mb-10">
-                    <img className="mx-auto" src={casestudy1} width="350" height="347" alt="Singtel Casestudy" />
-                    <div className="mt-6 text-center"><a href="#" className="inline-flex px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700">View case study</a></div>
-                </div>
-                <div className="md:w-1/3 sm:px-6 w-full mb-10">
-                    <img className="mx-auto" src={casestudy2} width="350" height="347" alt="Singtel Casestudy" />
-                    <div className="mt-6 text-center"><a href="#" className="inline-flex px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700">View case study</a></div>
-                </div>
-                <div className="md:w-1/3 sm:px-6 w-full mb-10">
-                    <img className="mx-auto" src={casestudy3} width="350" height="347" alt="Singtel Casestudy" />
-                    <div className="mt-6 text-center"><a href="#" className="inline-flex px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700">View case study</a></div>
-                </div>
-            </div>
+          <div className="md:w-1/3 sm:px-6 w-full mb-10">
+            <img className="mx-auto" src={casestudy2} width="350" height="347" alt="Singtel Casestudy" />
+            <div className="mt-6 text-center"><Link to="#" className="btn btn-small btn-blue">View case study</Link></div>
+          </div>
+          <div className="md:w-1/3 sm:px-6 w-full mb-10">
+            <img className="mx-auto" src={casestudy3} width="350" height="347" alt="Singtel Casestudy" />
+            <div className="mt-6 text-center"><Link to="#" className="btn btn-small btn-blue">View case study</Link></div>
+          </div>
         </div>
       </div>
-    )
-  }
-  
+    </div>
+  )
+}
--- src/components/Homepage/cloudComputing.js
+++ src/components/Homepage/cloudComputing.js
@@ -1,4 +1,5 @@
-import * as React from "react"
+import React from "react"
+import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 
 export default function CloudComputing() {
@@ -6,7 +7,7 @@ export default function CloudComputing() {
     <div className="relative bg-white pt-20 pb-32 overflow-hidden">
       <div className="relative">
         <div className="lg:mx-auto lg:max-w-6xl lg:px-8 lg:grid lg:grid-cols-2 lg:grid-flow-col-dense lg:gap-24">
-        <div className="mt-12 sm:mt-16 lg:mt-0">
+          <div className="mt-12 sm:mt-16 lg:mt-0">
             <div className="px-4 sm:pl-6 lg:px-0 lg:m-0 lg:relative lg:h-full">
               <StaticImage
                 imgClassName="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
@@ -20,19 +21,15 @@ export default function CloudComputing() {
               />
             </div>
           </div>
-          <div className="px-4 max-w-6xl mx-auto sm:px-6 lg:py-16 lg:max-w-none lg:mx-0 lg:px-0">
-            <div>
-              <div className="mt-6">
-                <h2 className="text-4xl font-extrabold tracking-tight text-gray-900">
-                  Cloud Computing
-                </h2>
-                <h4 className="max-w-lg mt-2 text-xl font-bold text-gray-600">Local IT Services helping you access your content anywhere!</h4>
-                <p className="mt-5 text-normal leading-relaxed text-gray-600">The concept of cloud computing may seem like a relatively new thing but it has been in use for over 10 years. Cloud Computing is a secure server that can be accessed via the internet.</p>
-                <p className="mt-5 text-normal leading-relaxed text-gray-600">This allows users to access business applications and store data online without the need for onsite hardware and software. As internet security improves at an exponential rate and NBN gets rolled out across Australia, the Cloud becomes an even more attractive solution.</p>
-                <div className="mt-6">
-                  <a href="#" className="inline-flex px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700">Discover more</a>
-                </div>
-              </div>
+          <div className="mt-6 lg:mt-0 px-4 max-w-6xl mx-auto sm:px-6 lg:py-16 lg:max-w-none lg:mx-0 lg:px-0">
+            <h2 className="is-h1 text-gray-900">
+              Cloud Computing
+            </h2>
+            <p className="max-w-lg text-xl font-bold text-gray-900">Local IT Services helping you access your content anywhere!</p>
+            <p className="mt-5">The concept of cloud computing may seem like a relatively new thing but it has been in use for over 10 years. Cloud Computing is a secure server that can be accessed via the internet.</p>
+            <p className="mt-5">This allows users to access business applications and store data online without the need for onsite hardware and software. As internet security improves at an exponential rate and NBN gets rolled out across Australia, the Cloud becomes an even more attractive solution.</p>
+            <div className="mt-6">
+              <Link to="#" className="btn btn-blue">Discover more</Link>
             </div>
           </div>
         </div>
