b5b1ad5eb86ba8dd3a6fa8799a2b3cc98d8410c8
Joe <joe@startdigital.com.au>
2021-09-13 12:36:46 +0800

    fix testimonials layout

--- src/pages/about/testimonials.js
+++ src/pages/about/testimonials.js
@@ -6,22 +6,6 @@ import Quote from "../../components/Quote"
 export default function Testimonials() {
   const testimonials = [
     {
-      quote: "We cannot speak highly enough of the staff assigned to our implementation. Our new infrastructure more complex than our previous systems. We required careful assistance to ensure there were no issues when we went ‘live’. At every step, iQuest team were helpful, approachable, and knowledgeable. The ongoing excellent support is really something we sincerely appreciate and rely on.",
-      company: "Fremantle Dockers",
-      image: {
-        src: "https://dev.iquest.com.au/wp-content/uploads/2021/06/Fremantle-Dockers-Logo.png",
-        alt: "Fremantle Dockers"
-      }
-    },
-    {
-      quote: "iQuest has been the most reliable, knowledgeable and consistent company I can ever remember working with in the IT sector",
-      company: "Giorgi",
-      image: {
-        src: "https://dev.iquest.com.au/wp-content/uploads/2021/06/Gorgi-Logo.png",
-        alt: "Giorgi"
-      }
-    },
-    {
       quote: "At Argonaut, we need to have confidence that the company has the technology infrastructure and support to maintain its business consistently and reliably. iQuest IT Support gives us that confidence…",
       company: "Argonaut",
       image: {
@@ -38,6 +22,14 @@ export default function Testimonials() {
       }
     },
     {
+      quote: "We cannot speak highly enough of the staff assigned to our implementation. Our new infrastructure more complex than our previous systems. We required careful assistance to ensure there were no issues when we went ‘live’. At every step, iQuest team were helpful, approachable, and knowledgeable. The ongoing excellent support is really something we sincerely appreciate and rely on.",
+      company: "Fremantle Dockers",
+      image: {
+        src: "https://dev.iquest.com.au/wp-content/uploads/2021/06/Fremantle-Dockers-Logo.png",
+        alt: "Fremantle Dockers"
+      }
+    },
+    {
       quote: "We have been using iQuest IT Support to look after our 3 offices in Perth for more than 5 years now. They are a great partner to have, almost a natural extension to our business as iQuest takes an interest and the time to understand our business…",
       company: "Florace Pty Ltd",
       image: {
@@ -45,21 +37,26 @@ export default function Testimonials() {
         alt: "Floraco Pty Ltd"
       }
     },
+    {
+      quote: "iQuest has been the most reliable, knowledgeable and consistent company I can ever remember working with in the IT sector",
+      company: "Giorgi",
+      image: {
+        src: "https://dev.iquest.com.au/wp-content/uploads/2021/06/Gorgi-Logo.png",
+        alt: "Giorgi"
+      }
+    }
   ];
 
   return (
     <>
       <PageTransition>
         <InternalLayout title="Our Testimonials">
-          <div className="wave-divider"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none"><path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" className="shape-fill"></path></svg></div>
-          <section className="bg-white">
-            <h2 className="max-w-6xl mx-auto">Hear from iQuest's partners and clients</h2>
-            <div className="max-w-6xl mx-auto md:grid md:grid-cols-2 md:px-6 lg:px-8">
-              {testimonials.map(({ quote, company, image }) => (
-                <Quote quote={quote} company={company} image={image} />
-              ))}
-            </div>
-          </section>
+          <h2>Hear from iQuest's partners and clients</h2>
+          <div className="md:grid md:grid-cols-2 md:px-6 lg:px-8">
+            {testimonials.map(({ quote, company, image }) => (
+              <Quote quote={quote} company={company} image={image} />
+            ))}
+          </div>
         </InternalLayout>
       </PageTransition>
     </>
